9dec8069220a91fa40f190be19dc65e8dde9aeb6
amantajatii <diazamantajatisusilo@mail.ugm.ac.id>
2025-05-29 00:07:26 +0700

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+let namaGuru = ['jodi', 'bunga', 'rahman']
+
+namaGuru.push('dea', 'fikri') // menambah ke elemen terakhir
+namaGuru.shift() // menghapus elemen pertama
+namaGuru.pop() // menghapus elemen terakhir
+
+alert(namaGuru[0])
