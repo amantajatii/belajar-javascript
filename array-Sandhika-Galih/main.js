9b0cf734e76e49c12dceeb468f0a0d5c75895315
amantajatii <diazamantajatisusilo@mail.ugm.ac.id>
2025-05-29 18:40:06 +0700

    array sandhika galih

---
+++ array-Sandhika-Galih/main.js
@@ -0,0 +1,16 @@
+/* 
+arr.length -> returns the number of elements in the array
+arr.join(separator) -> joins all elements of the array into a string, separated by the specified separator
+arr.push(value) -> adds a new element to the end of the array
+arr.pop() -> removes the last element from the array and returns it
+arr.shift() -> removes the first element from the array and returns it
+arr.unshift(value) -> adds a new element to the beginning of the array
+arr.slice(start, end) -> returns a shallow copy of a portion of the array from start to end (not inclusive)
+arr.splice(indexAwal, mauDihapusBerapa, item1, item2, ...) -> changes the contents of an array by removing or replacing existing elements and/or adding new elements in place
+arr.forEach(callback) -> executes a provided function once for each array element
+arr.map(callback) -> creates a new array with the results of calling a provided function on every element in the calling array
+arr.sort(compareFunction) -> sorts the elements of an array in place and returns the sorted array
+arr.filter(callback) -> creates a new array with all elements that pass the test implemented by the provided function. Filter bisa mencari banyak nilai dan memasukkan ke dalam array baru
+arr.find(callback) -> returns the value of the first element in the array that satisfies the provided testing function. find hanya mencari satu nilai yang sesuai dengan kondisi yang diberikan
+
+**/
