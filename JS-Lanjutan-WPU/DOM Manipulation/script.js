40088f7b2da144e9e3f520c05af774605738cc66
Diaz Amantajati Susilo <diazamantajatisusilo@mail.ugm.ac.id>
2025-06-05 11:29:37 +0700

    DOM Manipulation done

---
+++ JS-Lanjutan-WPU/DOM Manipulation/script.js
@@ -0,0 +1,14 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Belajar Javascript dan DOM</em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = '<div><p>Hello World</p></div>';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'Sandhika');
+
+const p2 = document.querySelector('.p2');
