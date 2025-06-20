ae643acf2eac3657ba3f7d44a3a7871945c571fd
DIAZZ <diazamantajatisusilo@mail.ugm.ac.id>
2025-06-20 21:39:48 +0700

    q

--- Javascript-BroCode/typeConversion/index.js
+++ Javascript-BroCode/typeConversion/index.js
@@ -1,5 +1,17 @@
-let age = window.prompt("How old are you?");
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age += 1;
 
-age += 1;
+// console.log(age);
 
-console.log(age);
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
