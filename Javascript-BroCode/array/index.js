fe6bbb6e2b5f58cc36ef9d6514565ccbcebff023
DIAZZ <diazamantajatisusilo@mail.ugm.ac.id>
2025-06-20 21:53:35 +0700

    array done

---
+++ Javascript-BroCode/array/index.js
@@ -0,0 +1,32 @@
+let fruits = ["apple", "orange", "banana"];
+
+console.log(fruits.length);
+
+// for(let i = 0 ; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+fruits.sort().reverse();
+
+for(let fruit of fruits) {
+    console.log(fruit);
+}
+
+// fruits[2] = "coconut";
+
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+
+
+// console.log(numOfFruits)
+// console.log(index);
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
